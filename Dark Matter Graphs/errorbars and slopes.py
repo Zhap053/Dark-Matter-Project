22e7a95807f06d4eeac403a742c9bf1ce1a209d0
zhap053 <zhap053@live.rhul.ac.uk>
2020-06-08 18:49:47 +0000

    correct rc and p0 on all graphs

--- Dark Matter Graphs/errorbars and slopes.py
+++ Dark Matter Graphs/errorbars and slopes.py
@@ -23,12 +23,12 @@ chi2=(y-fx_estimate)*(y-fx_estimate)/np.power(yerror,2)
 #print(chi2.sum())	#shows every chi value
 
 for slope in np.arange(start, stop, step):
-      fx=slope*x
-      chi2=np.sum(((y-fx)*(y-fx))/np.power(yerror,2))
-      if(chi2<minchi2):
-          minchi2=chi2
-          minslope=slope
-      #print(slope,chi2,minslope,minchi2)
+  fx=slope*x
+  chi2=np.sum(((y-fx)*(y-fx))/np.power(yerror,2))
+  if(chi2<minchi2):
+    minchi2=chi2
+    minslope=slope
+      print(slope,chi2,minslope,minchi2)
 print('The optimised bestfit graident is: ',minslope) #prints optimised graident for best fit
 fx_optimised=minslope*x
 
